10660da51db82cc7233e1af601779c96db26d4be
ell_1010 <elliottbarrett25@gmail.com>
2025-03-30 14:55:57 +0100

    fixed the incorrect LED numbers on all the config.h files so openRGB should work for all of them now (blame glorious, again))

--- keyboards/gmmk/gmmk3/p75/iso/config.h
+++ keyboards/gmmk/gmmk3/p75/iso/config.h
@@ -14,28 +14,10 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#pragma once
+ #pragma once
 
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
-/* External spi flash */
-#define EXTERNAL_FLASH_SPI_SLAVE_SELECT_PIN C4
-
-/* SPI Config for LED Driver */
-#define SPI_DRIVER SPIDQ
-#define SPI_SCK_PIN A5
-#define SPI_MOSI_PIN A7
-#define SPI_MISO_PIN A6
-
-#define SPI_SCK_PAL_MODE 5
-#define SPI_MOSI_PAL_MODE 5
-#define SPI_MISO_PAL_MODE 5
-
-#define AW20216S_CS_PIN_1 C5
-#define AW20216S_CS_PIN_2 B11
-#define AW20216S_EN_PIN   C13
-
-#define DIP_SWITCH_PINS { A13 }
+ /* Driver LED totals (different for each board)*/
+ #define DRIVER_1_LED_TOTAL (66)
+ #define DRIVER_2_LED_TOTAL (36)
+ #define RGB_MATRIX_LED_COUNT (DRIVER_1_LED_TOTAL + DRIVER_2_LED_TOTAL)
+ #define DRIVER_LED_TOTAL (DRIVER_1_LED_TOTAL + DRIVER_2_LED_TOTAL)
