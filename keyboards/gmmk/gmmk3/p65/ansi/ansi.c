4f221ef6749bacf72df07bfa6c3c8a62a4df53cc
Travis Baker <baker.travis.w@gmail.com>
2025-04-05 11:44:15 -0600

    update leds for esc and badge

--- keyboards/gmmk/gmmk3/p65/ansi/ansi.c
+++ keyboards/gmmk/gmmk3/p65/ansi/ansi.c
@@ -30,7 +30,7 @@
  *   |     |          |          B location
  *   |     |          |          | */
 const aw20216s_led_t PROGMEM g_aw20216s_leds[AW20216S_LED_COUNT] = {
-    {0, SW1_CS1, SW1_CS2, SW1_CS3},        // 0 ESC
+    {0, SW1_CS4, SW1_CS5, SW1_CS6},        // 0 ESC
     {0, SW2_CS4, SW2_CS5, SW2_CS6},        // 1 1
     {0, SW3_CS4, SW3_CS5, SW3_CS6},        // 2 2
     {0, SW4_CS4, SW4_CS5, SW4_CS6},        // 3 3
@@ -121,6 +121,7 @@ const aw20216s_led_t PROGMEM g_aw20216s_leds[AW20216S_LED_COUNT] = {
     {1, SW8_CS16, SW8_CS17, SW8_CS18},     // 82 SLED18
     {1, SW9_CS16, SW9_CS17, SW9_CS18},     // 83 SLED19
     {1, SW10_CS16, SW10_CS17, SW10_CS18},  // 84 SLED20
+    {1, SW10_CS1, SW10_CS2, SW10_CS3},     // Badge
 };
 
 led_config_t g_led_config = {
@@ -149,6 +150,7 @@ led_config_t g_led_config = {
         // Left and right side lights
         {0, 15},  {0, 20},  {0, 25},  {0, 30},  {0, 35},  {0, 40},  {0, 45},  {0, 50},  {0, 55},  {0, 60},
         {224, 15},  {224, 20},  {224, 25},  {224, 30},  {224, 35},  {224, 40},  {224, 45},  {224, 50},  {224, 55},  {224, 60},
+        {214, 0}, //Badge
     },
     {
         // Keyboard RGB Matrix
@@ -160,6 +162,7 @@ led_config_t g_led_config = {
         // Left and right side lights
         2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
         2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+        8, //Badge
     }
 };
 #endif // RGB_MATRIX_ENABLE
